f8d9d21a53be4adcc4bf665a9535e578e2b825c5
saichaitanya292 <saichaitanya292@gmail.com>
2023-01-30 23:29:19 +0530

    challenge5 timer and clicks

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEvent, Observable } from 'rxjs';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-about',
@@ -6,15 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  clickObservable: Observable<Event> = fromEvent(document,'click');
+
+
 
   constructor() { }
-
   ngOnInit(): void {
+    this.subscribeToObservable();
+    this.subscribeToObservablenum();
   }
+  private subscribeToObservable() {
+      this.clickObservable.subscribe(() => { 
+      console.log(`The dom has been clicked!`);
+    })
+
+  }
+
+  private subscribeToObservablenum() {
+    const source = timer(1000, 2000);
+    source.subscribe(val => console.log(val));
+  
+}
+
 
   public nearmealerthome(event:any)
   {
     alert(event.target.value)
   }
+
+
 
 }
